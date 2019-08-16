3bfe09fc474d929ab4ec2ed3ad8e16f20bb35266
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-08-16 10:08:53 -0600

    Refactor all JS files

--- src/Game.js
+++ src/Game.js
@@ -1,15 +1,16 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
-const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 const Round = require('../src/Round');
 const Deck = require('../src/Deck');
 
 class Game {
-  constructor() { }
+  constructor() {
+    this.roundCount = 0;
+  }
 
-  printMessage(deck, round) {
+  printMessage(deck) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
@@ -19,10 +20,11 @@ class Game {
   }
 
   start() {
-    const deck = new Deck(prototypeQuestions.map((data) => new Card({ ...data })));
-    const round = new Round(deck, this)
+    const deck = new Deck(prototypeQuestions.map(data => new Card(data)));
+    const round = new Round(deck)
     this.printMessage(deck);
     this.printQuestion(round);
+    this.round++;
   }
 }
 
--- test/Game-test.js
+++ test/Game-test.js
@@ -4,7 +4,6 @@ const expect = chai.expect;
 const Round = require('../src/Round');
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
-const Turn = require('../src/Turn');
 const Game = require('../src/Game');
 
 let card1, card2, card3, deck, round, game;
